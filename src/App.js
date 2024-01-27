22306b6956e5a3abb2e5f5669aab030d9f16cfa2
Payalsasmal10 <sasmalpayal@gmail.com>
2024-01-27 16:54:01 +0530

    reset is working as expected

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import './App.css';
 function App() {
   const [countLaps, setCountLaps] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [isPlayAllRunning, setIsPlayAllRunning] = useState(false);
   const [timerID, setTimerID] = useState(null);
 
-  const startCounterHandler = () => {
-    if(!isRunning){
-      const timerId = () => {
-        setCountLaps((prvBar) => prvBar+1);
-        setTimerID(setTimeout(timerId, 1000));
-      }
+  const startCounterHandler = (e) => {
+    const buttonName= e.target.name;
+    const timerId = () => {
+      setCountLaps((prvBar) => prvBar+1);
+      setTimerID(setTimeout(timerId, 1000));
+    }
+    if((!isRunning && buttonName === 'singlePlayButton')){
       timerId();
-    }else{
+      setIsRunning(!isRunning);
+    }else if(!isPlayAllRunning && buttonName === "playAllButton"){
+      timerId();
+      setIsPlayAllRunning(!isPlayAllRunning);
+    }
+    else{
 
       clearInterval(timerID);
     }
-    setIsRunning(!isRunning);
     console.log("I am running");
   };
 
-  const restChangeHandler = () => {
+  const resetChangeHandler = () => {
     clearInterval(timerID);
     setCountLaps(0);
     console.log("reset all");
@@ -29,11 +35,11 @@ function App() {
 
   return (
     <div className="App">
-      <button>{!isRunning? 'Play All': 'Resume'}</button>
-      <button onClick={restChangeHandler}>Rest All</button>
+      <button name="playAllButton" onClick={startCounterHandler}>{!isPlayAllRunning? 'Play All': 'Resume'}</button>
+      <button onClick={resetChangeHandler}>Reset All</button>
         <div className="outer-container">
           <div>{countLaps}</div>
-          <button onClick={startCounterHandler}>{!isRunning ? 'Start':'Pause'}</button>
+          <button onClick={startCounterHandler} name="singlePlayButton">{!isRunning ? 'Start':'Pause'}</button>
         </div>
     </div>
   );

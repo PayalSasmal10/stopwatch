ccd3775e5edc7a45e426943259b2a9989531796d
Payalsasmal10 <sasmalpayal@gmail.com>
2024-01-27 13:30:06 +0530

    starting thecounter is working as expected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const [countLaps, setCountLaps] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const startCounterHandler = () => {
+    const timerId = setInterval(() => {
+      setCountLaps((prvBar) => prvBar+1);
+      
+    }, 1000);
+    setIsRunning(true);
+    return () => clearInterval(timerId);
+  };
+
+  const pauseCounter = () => {
+    
+  };
+   
   return (
     <div className="App">
-              
+        <div className="outer-container">
+          <div>{countLaps}</div>
+          <button onClick={startCounterHandler}>{!isRunning ? 'Start':'Pause'}</button>
+        </div>
     </div>
   );
 }
